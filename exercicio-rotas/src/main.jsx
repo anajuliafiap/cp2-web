6f76b0c09c1dbab6bea28048d89fb422e942a215
anajuliafiap <anajuliaalmeida7654@gmail.com>
2023-09-06 18:40:26 -0300

    inclui os componentes solicitados

--- exercicio-rotas/src/main.jsx
+++ exercicio-rotas/src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./routes/Home/index.jsx"
+import Produtos from "./routes/Produtos/index.jsx"
+import Error from "./routes/Error/index.jsx"
+import EditarProduos from "./routes/EditarProdutos//index.jsx"
