26f8731ccff02440270093046033ce4c9d7d39c3
anajuliafiap <anajuliaalmeida7654@gmail.com>
2023-09-06 19:43:38 -0300

    arrumei produtos.jsx, passando os valores neve

--- exercicio-rotas/src/routes/Produtos/index.jsx
+++ exercicio-rotas/src/routes/Produtos/index.jsx
@@ -1,8 +1,17 @@
-export default function Produtos(){
+import { Link } from "react-router-dom"
+import { listaProdutos } from "/Users/anajuliavianna/Documents/cp2-web/exercicio-rotas/src/routes/Produtos/todosProdutos.js"
 
+export default function Produtos(){
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>Editar o produto: {prod.nome}</Link>
+                </div>
+            ))
+
+            }
         </main>
     )
 }
